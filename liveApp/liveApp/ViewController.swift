4c25d4f1bfa07ba93982e7a11e6b7f33d3ee6737
酒窝 <xuan6zm@163.com>
2026-07-08 15:02:54 +0800

    修改：搭建项目

---
+++ liveApp/liveApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  liveApp
+//
+//  Created by mac on 2026/7/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
